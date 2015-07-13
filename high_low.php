8119cce35281ba69abaa12416ca28178c99a6d87
Dylan Perenchio <perenchiod@gmail.com>
2015-07-13 14:16:52 -0500

    High and low guessing game in PHP

---
+++ high_low.php
@@ -0,0 +1,26 @@
+<?php 
+
+	$randomVar = rand(1,100);
+	$tries = 0;
+	fwrite(STDOUT, "Guess a number between 1-100 and get hints to find the random number." . PHP_EOL);
+	
+	do {
+		do {
+			$tries++;
+			fwrite(STDOUT, "Guess: ");
+			$userVar = fgets(STDIN);
+			if($userVar > $randomVar) {
+				fwrite(STDOUT, "You guessed high" . PHP_EOL);
+			}
+			if($userVar < $randomVar) {
+				fwrite(STDOUT, "You guess low" . PHP_EOL);
+			}
+		}while($randomVar != $userVar);
+		
+		fwrite(STDOUT, "You got it! Number of tries taken: $tries" . PHP_EOL . "Would you like to play again (y/n): ");
+		$userAns = trim(fgets(STDIN));
+		echo $userAns;
+	}while($userAns == "y");
+
+
+?>
