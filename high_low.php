a16ab7f0e33c218416bcf81b55f1f28b1baa92d1
Dylan Perenchio <perenchiod@gmail.com>
2015-07-13 16:01:25 -0500

    some updates

--- high_low.php
+++ high_low.php
@@ -1,26 +1,38 @@
 <?php 
-
-	fwrite(STDOUT, "Guess a number between 1-100 and get hints to find the random number." . PHP_EOL);
-	
+	fwrite(STDOUT, "Guess a number between the parameters you enter and get hints to find the random number." . PHP_EOL);
 	do {
-	$randomVar = rand(1,100);
-	$tries = 0;
-		do {
-			$tries++;
-			fwrite(STDOUT, "Guess: ");
-			$userVar = fgets(STDIN);
-			if($userVar > $randomVar) {
-				fwrite(STDOUT, "You guessed high" . PHP_EOL);
-			}
-			if($userVar < $randomVar) {
-				fwrite(STDOUT, "You guess low" . PHP_EOL);
-			}
-		}while($randomVar != $userVar);
-		
-		fwrite(STDOUT, "You got it! Number of tries taken: $tries" . PHP_EOL . "Would you like to play again (y/n): ");
-		$userAns = trim(fgets(STDIN));
-		echo $userAns;
-	}while($userAns == "y");
+		fwrite(STDOUT, "The parameter you set to play with is: $argv[1] , $argv[2]" . PHP_EOL);
+		$randomVar = rand($argv[1],$argv[2]);
+		$tries = 0;
+			do {
+				//Stops loop if user enters too many times
+				if($tries == 5){
+					fwrite(STDOUT , "TOO MANY TRIES" . PHP_EOL);
+					break;
+				}
+				fwrite(STDOUT, "Guess: ");
+				$userVar = fgets(STDIN);
 
+				if($userVar > $randomVar) {
+					fwrite(STDOUT, "You guessed high" . PHP_EOL);
+				} else {
+					fwrite(STDOUT, "You guess low" . PHP_EOL);
+				}
+
+				//Gives user warning and hint after set amount of tries
+				if($tries == 3) {
+					$hint = substr($randomVar, -1);
+					fwrite(STDOUT, "1 guess remaining :X". PHP_EOL. "Here's a hint... the last number is ". $hint . PHP_EOL);
+				}
+				//Runs when the user guesses the number correctly
+				if($userVar == $randomVar) {
+					fwrite(STDOUT, "You got it! Number of tries taken: $tries" . PHP_EOL);
+				}
+				$tries++;
+			}while($randomVar != $userVar);
+			
+			fwrite(STDOUT, "Would you like to play again (y/n): ");
+			$userAns = trim(fgets(STDIN));
+	}while($userAns == "y" || $userAns == "Y");
 
 ?>
